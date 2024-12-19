0fc31ba9e56af855627c1ea29e94e8087d84a2ed
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-19 16:36:26 +0100

    6 - Adds JS

---
+++ main.js
@@ -0,0 +1,9 @@
+const navMenu = document.getElementById("nav-menu"),
+navToggle = document.getElementById("nav-toggle"),
+navClose = document.getElementById("nav-close")
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add("show-menu")
+    })
+}
