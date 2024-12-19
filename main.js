9cae23e2981d18e65b25c730ffac951f9ed5a2a1
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-19 19:33:45 +0100

    7 - ads scroll reveal

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ if(navToggle) {
         navMenu.classList.add("show-menu")
     })
 }
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+//Scroll reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 350,
+    reset: true
+})
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+
+// ScrollReveal().reveal('.home-swiper');
