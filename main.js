b7f54c663b047eebdf4b0f8186f3086809b808a4
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-21 14:58:52 +0100

    11

--- main.js
+++ main.js
@@ -38,6 +38,14 @@ let newSWiper = new Swiper(".new-swiper", {
     spaceBetween: 16,
 })
 
+//Scroll up
+function scrollUp() {
+    const scrollup = document.getElementById("scroll-up")
+    if(this.scrollY>=460) scrollup.classList.add("show-scroll");
+    else scrollup.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollUp)
+
 //Scroll reveal
 const sr = ScrollReveal({
     origin: 'top',
